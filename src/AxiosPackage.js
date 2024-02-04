4d81d449ab7881e6823e9d5ca191302650efe469
mahshidkhodsiani <mahshidkhodsiani@yahoo.com>
2024-02-04 14:24:39 +0330

    004-mahshid

--- src/AxiosPackage.js
+++ src/AxiosPackage.js
@@ -2,23 +2,53 @@ import React, { useEffect } from 'react';
 import axios from "axios";
 
 const BASE_URL = 'https://dummyjson.com';
+const BASE_URL2 = 'https://jsonplaceholder.typicode.com/posts/1';
+const fakeData ={title : "mahshid", description: "khodsiani", price : 123};
 
 const AxiosPackage = () => {
 
     useEffect(() => {
         getData();
+        // postData(fakeData);
+        // deleteData(2);
     }, []
 
     );
 
     const getData = () =>{
-        axios.get(`${BASE_URL}/products/2`).then((response) => console.log(response.data))
+        axios.get(`${BASE_URL}/products/1`).
+        then((response) => console.log(response.data))
         .catch((error) => console.log(error.message));
-    }
+    };
+
+    const postData = (data) =>{
+        axios.post(`${BASE_URL}/products/add` , data).
+        then((response) => console.log(response.data))
+        .catch((error) => console.log(error.message));
+    };
+
+    const deleteData = (id) =>{
+        axios.delete(`${BASE_URL}/products/${id}`).
+        then((response) => console.log(response.data))
+        .catch((error) => console.log(error.message));
+    };
+
+    const updatePutData = (id, data) =>{
+        axios.put(`${BASE_URL}/products/${id}`, data).
+        then((response) => console.log(response.data))
+        .catch((error) => console.log(error.message));
+    };
+
+    const updatePatchData = (id, data) =>{
+        axios.patch(`${BASE_URL}/products/${id}`, data).
+        then((response) => console.log(response.data))
+        .catch((error) => console.log(error.message));
+    };
 
     return (
         <div>
-
+            <button onClick={() => updatePutData(1 , fakeData)} >PUT DATA</button>
+            <button onClick={() => updatePatchData(1 , fakeData)}>PATCH DATA</button>
         </div>
     );
 }
